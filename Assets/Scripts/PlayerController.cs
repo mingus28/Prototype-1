f914d47112240acc6fca96f78b2d9a2ac2e0c0f8
Mingus de Geest <329869@student.mboutrecht.nl>
2020-11-06 13:48:54 +0100

    Eerste push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public float turnSpeed = 30f;
+    
+    float horizontalInput;
+
+    float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * horizontalInput * turnSpeed);
+    }
+}
